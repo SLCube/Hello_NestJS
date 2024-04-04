f4872785786c14bc15df963c1403c63c65957420
SLCube <kgm0096@naver.com>
2024-04-04 14:20:23 +0900

    NestJS MiddleWare 학습

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Hello NestJS
+
+## [NestJS 공식문서를 보고 학습한 Repository](https://docs.nestjs.com/)
+
+### Java Spring과의 차이점.
+
+1. Java Spring에서의 Annotation을 데코레이터라 부름
+1. MiddleWare가 존재함. Spring으로 치면 filter정도의 역할인듯..?
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleWare } from './common/logger/logger.middleware';
 
 @Module({
   imports: [CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleWare)
+      .forRoutes({path : 'cats', method : RequestMethod.GET});
+  }
+}
