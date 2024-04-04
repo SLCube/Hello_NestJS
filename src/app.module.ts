0b37f48fcaec0fdcc2b812a66e8dfe52075a5c35
SLCube <kgm0096@naver.com>
2024-04-04 14:38:39 +0900

    NestJS ExceptionFilter 학습

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleWare } from './common/logger/logger.middleware';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from './common/filter/http-Exception.filter';
 
 @Module({
   imports: [CatsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, 
+    {
+      provide : APP_FILTER, 
+      useClass : HttpExceptionFilter
+    }],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
